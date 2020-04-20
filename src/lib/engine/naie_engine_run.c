e5ee9ad08db80561b54ffa48cf9db11db9a6b06b
KJ <kees.jan.hermans@gmail.com>
2020-04-20 18:21:02 +0200

    Func doc needed

--- src/lib/engine/naie_engine_run.c
+++ src/lib/engine/naie_engine_run.c
@@ -10,12 +10,20 @@
  * \brief
  */
 
-#include <ctype.h>
-
 #include <naigama/engine/naie.h>
 
 /**
+ * Runs the bytecode, from the beginning, against the input,
+ * also from the beginning. Ends when either a FAIL condition
+ * eats up the entire stack (no match), or the 'end' instruction
+ * is encountered (natch). Or an abnormal situation is encountered.
  *
+ * \param engine  Initialized engine
+ * \param result  Uninitialized result, contains all relevant
+ *                end code / capture data / replace data
+ *                when this function returns.
+ * \returns       NAIG_OK on success, NAIG_FAILURE on no match, or
+ *                any other error on abnormal end.
  */
 NAIG_ERR_T naie_engine_run
   (

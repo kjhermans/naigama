78593216efb7db0239b041899ff3192c8bf7f0b2
KJ <kees.jan.hermans@gmail.com>
2020-04-25 20:05:18 +0200

    Overarching API gets replacement skeleton code.

---
+++ src/lib/naigama/naig_replace.c
@@ -0,0 +1,29 @@
+/**
+ * This file is part of Naigama, a parser engine.
+ * Copyright 2020, KJ Hermans
+ * Written by KJ Hermans <kees.jan.hermans@gmail.com>
+ *
+ * LICENSE:
+ * Withheld
+ *
+ * \file
+ * \brief
+ */
+
+#include <naigama/naigama.h>
+
+/**
+ * Returns a malloc'ed buffer which has processed the replacements
+ * inside the result list.
+ */
+NAIG_ERR_T naig_replace
+  (
+    naig_t* naig,
+    unsigned char* input,
+    unsigned input_length,
+    naig_result_t* result,
+    unsigned char** output,
+    unsigned* output_length
+  )
+{
+}

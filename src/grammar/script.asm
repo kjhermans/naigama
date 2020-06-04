3090b916496f158f05d87f70ef92d4ef0f3f7899
KJ <kees.jan.hermans@gmail.com>
2020-06-04 16:47:21 +0200

    Script grammar with better capture regions.

--- src/grammar/script.asm
+++ src/grammar/script.asm
@@ -212,21 +212,17 @@ __SUCCESS_582:
 __RULE_VARDECL:
   call __RULE___prefix
   call __RULE_KW_VAR
-  opencapture 10
   call __RULE_IDENT
-  closecapture 10
-  catch __FORGIVE_629
+  catch __FORGIVE_622
   call __RULE_ASSIGN
   call __RULE_EXPRESSION
-  commit __FORGIVE_629
-__FORGIVE_629:
+  commit __FORGIVE_622
+__FORGIVE_622:
   call __RULE_SEMICOLON
   ret
 __RULE_ASSIGNMENT:
   call __RULE___prefix
-  opencapture 11
   call __RULE_IDENT
-  closecapture 11
   call __RULE_ASSIGNS
   call __RULE_EXPRESSION
   call __RULE_SEMICOLON
@@ -234,239 +230,234 @@ __RULE_ASSIGNMENT:
 __RULE_EXPRESSION:
   call __RULE___prefix
   call __RULE_EXPR2
-  catch __FORGIVE_699
-__ENDLESS_698:
-  opencapture 12
-  catch __RIGHTHAND_709
+  catch __FORGIVE_685
+__ENDLESS_684:
+  catch __RIGHTHAND_695
   call __RULE_AND
-  commit __SUCCESS_709
-__RIGHTHAND_709:
+  commit __SUCCESS_695
+__RIGHTHAND_695:
   call __RULE_OR
-__SUCCESS_709:
-  closecapture 12
+__SUCCESS_695:
   call __RULE_EXPR2
-  partialcommit __ENDLESS_698
-__FORGIVE_699:
+  partialcommit __ENDLESS_684
+__FORGIVE_685:
   ret
 __RULE_EXPR2:
   call __RULE___prefix
   call __RULE_EXPR3
-  catch __FORGIVE_742
-__ENDLESS_741:
-  opencapture 13
-  catch __RIGHTHAND_752
+  catch __FORGIVE_728
+__ENDLESS_727:
+  catch __RIGHTHAND_738
   call __RULE_BITAND
-  commit __SUCCESS_752
-__RIGHTHAND_752:
-  catch __RIGHTHAND_760
+  commit __SUCCESS_738
+__RIGHTHAND_738:
+  catch __RIGHTHAND_746
   call __RULE_BITOR
-  commit __SUCCESS_760
-__RIGHTHAND_760:
+  commit __SUCCESS_746
+__RIGHTHAND_746:
   call __RULE_BITXOR
-__SUCCESS_760:
-__SUCCESS_752:
-  closecapture 13
+__SUCCESS_746:
+__SUCCESS_738:
   call __RULE_EXPR3
-  partialcommit __ENDLESS_741
-__FORGIVE_742:
+  partialcommit __ENDLESS_727
+__FORGIVE_728:
   ret
 __RULE_EXPR3:
   call __RULE___prefix
   call __RULE_EXPR4
-  catch __FORGIVE_793
-__ENDLESS_792:
-  opencapture 14
-  catch __RIGHTHAND_803
+  catch __FORGIVE_779
+__ENDLESS_778:
+  catch __RIGHTHAND_789
   call __RULE_EQUALS
-  commit __SUCCESS_803
-__RIGHTHAND_803:
-  catch __RIGHTHAND_811
+  commit __SUCCESS_789
+__RIGHTHAND_789:
+  catch __RIGHTHAND_797
   call __RULE_NEQUALS
-  commit __SUCCESS_811
-__RIGHTHAND_811:
-  catch __RIGHTHAND_819
+  commit __SUCCESS_797
+__RIGHTHAND_797:
+  catch __RIGHTHAND_805
   call __RULE_LTEQ
-  commit __SUCCESS_819
-__RIGHTHAND_819:
-  catch __RIGHTHAND_827
+  commit __SUCCESS_805
+__RIGHTHAND_805:
+  catch __RIGHTHAND_813
   call __RULE_LT
-  commit __SUCCESS_827
-__RIGHTHAND_827:
-  catch __RIGHTHAND_835
+  commit __SUCCESS_813
+__RIGHTHAND_813:
+  catch __RIGHTHAND_821
   call __RULE_GTEQ
-  commit __SUCCESS_835
-__RIGHTHAND_835:
+  commit __SUCCESS_821
+__RIGHTHAND_821:
   call __RULE_GT
-__SUCCESS_835:
-__SUCCESS_827:
-__SUCCESS_819:
-__SUCCESS_811:
-__SUCCESS_803:
-  closecapture 14
+__SUCCESS_821:
+__SUCCESS_813:
+__SUCCESS_805:
+__SUCCESS_797:
+__SUCCESS_789:
   call __RULE_EXPR4
-  partialcommit __ENDLESS_792
-__FORGIVE_793:
+  partialcommit __ENDLESS_778
+__FORGIVE_779:
   ret
 __RULE_EXPR4:
   call __RULE___prefix
   call __RULE_EXPR5
-  catch __FORGIVE_868
-__ENDLESS_867:
-  opencapture 15
-  catch __RIGHTHAND_878
+  catch __FORGIVE_854
+__ENDLESS_853:
+  catch __RIGHTHAND_864
   call __RULE_POW
-  commit __SUCCESS_878
-__RIGHTHAND_878:
-  catch __RIGHTHAND_886
+  commit __SUCCESS_864
+__RIGHTHAND_864:
+  catch __RIGHTHAND_872
   call __RULE_MUL
-  commit __SUCCESS_886
-__RIGHTHAND_886:
+  commit __SUCCESS_872
+__RIGHTHAND_872:
   call __RULE_DIV
-__SUCCESS_886:
-__SUCCESS_878:
-  closecapture 15
+__SUCCESS_872:
+__SUCCESS_864:
   call __RULE_EXPR5
-  partialcommit __ENDLESS_867
-__FORGIVE_868:
+  partialcommit __ENDLESS_853
+__FORGIVE_854:
   ret
 __RULE_EXPR5:
   call __RULE___prefix
   call __RULE_EXPR6
-  catch __FORGIVE_919
-__ENDLESS_918:
-  opencapture 16
-  catch __RIGHTHAND_929
+  catch __FORGIVE_905
+__ENDLESS_904:
+  catch __RIGHTHAND_915
   call __RULE_ADD
-  commit __SUCCESS_929
-__RIGHTHAND_929:
+  commit __SUCCESS_915
+__RIGHTHAND_915:
   call __RULE_SUB
-__SUCCESS_929:
-  closecapture 16
+__SUCCESS_915:
   call __RULE_EXPR6
-  partialcommit __ENDLESS_918
-__FORGIVE_919:
+  partialcommit __ENDLESS_904
+__FORGIVE_905:
   ret
 __RULE_EXPR6:
   call __RULE___prefix
-  catch __FORGIVE_955
-__ENDLESS_954:
+  catch __FORGIVE_941
+__ENDLESS_940:
+  opencapture 10
   call __RULE_UNARY
-  partialcommit __ENDLESS_954
-__FORGIVE_955:
+  closecapture 10
+  partialcommit __ENDLESS_940
+__FORGIVE_941:
+  opencapture 11
   call __RULE_TERM
+  closecapture 11
   ret
 __RULE_UNARY:
   call __RULE___prefix
-  opencapture 17
-  catch __RIGHTHAND_979
+  catch __RIGHTHAND_965
   call __RULE_NOT
-  commit __SUCCESS_979
-__RIGHTHAND_979:
-  catch __RIGHTHAND_987
+  commit __SUCCESS_965
+__RIGHTHAND_965:
+  catch __RIGHTHAND_973
   call __RULE_INC
-  commit __SUCCESS_987
-__RIGHTHAND_987:
+  commit __SUCCESS_973
+__RIGHTHAND_973:
   call __RULE_DEC
-__SUCCESS_987:
-__SUCCESS_979:
-  closecapture 17
+__SUCCESS_973:
+__SUCCESS_965:
   ret
 __RULE_TERM:
   call __RULE___prefix
-  catch __RIGHTHAND_1002
-  opencapture 18
+  catch __RIGHTHAND_988
+  opencapture 12
   call __RULE_LITERAL
-  closecapture 18
-  commit __SUCCESS_1002
-__RIGHTHAND_1002:
-  catch __RIGHTHAND_1017
-  opencapture 19
+  closecapture 12
+  commit __SUCCESS_988
+__RIGHTHAND_988:
+  catch __RIGHTHAND_1003
+  opencapture 13
   call __RULE_IDENT
-  closecapture 19
+  closecapture 13
   call __RULE_BOPEN
-  catch __FORGIVE_1043
+  catch __FORGIVE_1029
   call __RULE_EXPRESSION
-  catch __FORGIVE_1057
-__ENDLESS_1056:
+  catch __FORGIVE_1043
+__ENDLESS_1042:
   call __RULE_COMMA
   call __RULE_EXPRESSION
-  partialcommit __ENDLESS_1056
-__FORGIVE_1057:
-  commit __FORGIVE_1043
+  partialcommit __ENDLESS_1042
 __FORGIVE_1043:
+  commit __FORGIVE_1029
+__FORGIVE_1029:
   call __RULE_BCLOSE
-  commit __SUCCESS_1017
-__RIGHTHAND_1017:
-  catch __RIGHTHAND_1081
-  opencapture 20
+  commit __SUCCESS_1003
+__RIGHTHAND_1003:
+  catch __RIGHTHAND_1067
+  opencapture 14
   call __RULE_IDENT
-  closecapture 20
-  commit __SUCCESS_1081
-__RIGHTHAND_1081:
+  closecapture 14
+  commit __SUCCESS_1067
+__RIGHTHAND_1067:
   call __RULE_BOPEN
-  opencapture 21
+  opencapture 15
   call __RULE_EXPRESSION
-  closecapture 21
+  closecapture 15
   call __RULE_BCLOSE
-__SUCCESS_1081:
-__SUCCESS_1017:
-__SUCCESS_1002:
+__SUCCESS_1067:
+__SUCCESS_1003:
+__SUCCESS_988:
   ret
 __RULE_LITERAL:
   call __RULE___prefix
-  catch __RIGHTHAND_1124
+  catch __RIGHTHAND_1110
   call __RULE_STRINGLITERAL
-  commit __SUCCESS_1124
-__RIGHTHAND_1124:
-  catch __RIGHTHAND_1132
+  commit __SUCCESS_1110
+__RIGHTHAND_1110:
+  catch __RIGHTHAND_1118
   call __RULE_HASHLITERAL
-  commit __SUCCESS_1132
-__RIGHTHAND_1132:
-  catch __RIGHTHAND_1140
+  commit __SUCCESS_1118
+__RIGHTHAND_1118:
+  catch __RIGHTHAND_1126
   call __RULE_LISTLITERAL
-  commit __SUCCESS_1140
-__RIGHTHAND_1140:
-  catch __RIGHTHAND_1148
+  commit __SUCCESS_1126
+__RIGHTHAND_1126:
+  catch __RIGHTHAND_1134
   call __RULE_FLOATLITERAL
-  commit __SUCCESS_1148
-__RIGHTHAND_1148:
-  catch __RIGHTHAND_1156
+  commit __SUCCESS_1134
+__RIGHTHAND_1134:
+  catch __RIGHTHAND_1142
   call __RULE_INTLITERAL
-  commit __SUCCESS_1156
-__RIGHTHAND_1156:
+  commit __SUCCESS_1142
+__RIGHTHAND_1142:
   call __RULE_BOOLEANLITERAL
-__SUCCESS_1156:
-__SUCCESS_1148:
-__SUCCESS_1140:
-__SUCCESS_1132:
-__SUCCESS_1124:
+__SUCCESS_1142:
+__SUCCESS_1134:
+__SUCCESS_1126:
+__SUCCESS_1118:
+__SUCCESS_1110:
   ret
 __RULE_STRINGLITERAL:
   call __RULE___prefix
+  opencapture 16
   char 27
-  catch __FORGIVE_1182
-__ENDLESS_1181:
-  catch __RIGHTHAND_1185
+  catch __FORGIVE_1175
+__ENDLESS_1174:
+  catch __RIGHTHAND_1178
   char 5c
-  catch __RIGHTHAND_1200
+  catch __RIGHTHAND_1193
   set 0000000080000000000000100040540000000000000000000000000000000000
-  commit __SUCCESS_1200
-__RIGHTHAND_1200:
+  commit __SUCCESS_1193
+__RIGHTHAND_1193:
   counter 0 3
-__COUNTER_1209:
+__COUNTER_1202:
   set 000000000000ff03000000000000000000000000000000000000000000000000
-  condjump 0 __COUNTER_1209
-__SUCCESS_1200:
-  commit __SUCCESS_1185
-__RIGHTHAND_1185:
+  condjump 0 __COUNTER_1202
+__SUCCESS_1193:
+  commit __SUCCESS_1178
+__RIGHTHAND_1178:
   set ffffffff7fffffffffffffefffffffffffffffffffffffffffffffffffffffff
-__SUCCESS_1185:
-  partialcommit __ENDLESS_1181
-__FORGIVE_1182:
+__SUCCESS_1178:
+  partialcommit __ENDLESS_1174
+__FORGIVE_1175:
   char 27
+  closecapture 16
   ret
 __RULE_HASHLITERAL:
   call __RULE___prefix
+  opencapture 17
   call __RULE_CBOPEN
   catch __FORGIVE_1240
   call __RULE_HASHELT
@@ -479,348 +470,453 @@ __FORGIVE_1254:
   commit __FORGIVE_1240
 __FORGIVE_1240:
   call __RULE_CBCLOSE
+  closecapture 17
   ret
 __RULE_HASHELT:
   call __RULE___prefix
+  opencapture 18
   call __RULE_HASHKEY
   call __RULE_COLON
   call __RULE_HASHVALUE
+  closecapture 18
   ret
 __RULE_HASHKEY:
   call __RULE___prefix
+  opencapture 19
   call __RULE_TERM
+  closecapture 19
   ret
 __RULE_HASHVALUE:
   call __RULE___prefix
+  opencapture 20
   call __RULE_TERM
+  closecapture 20
   ret
 __RULE_LISTLITERAL:
   call __RULE___prefix
+  opencapture 21
   call __RULE_ABOPEN
-  catch __FORGIVE_1324
+  catch __FORGIVE_1352
   call __RULE_LISTELT
-  catch __FORGIVE_1338
-__ENDLESS_1337:
+  catch __FORGIVE_1366
+__ENDLESS_1365:
   call __RULE_COMMA
   call __RULE_LISTELT
-  partialcommit __ENDLESS_1337
-__FORGIVE_1338:
-  commit __FORGIVE_1324
-__FORGIVE_1324:
+  partialcommit __ENDLESS_1365
+__FORGIVE_1366:
+  commit __FORGIVE_1352
+__FORGIVE_1352:
   call __RULE_ABCLOSE
+  closecapture 21
   ret
 __RULE_LISTELT:
   call __RULE___prefix
+  opencapture 22
   call __RULE_TERM
+  closecapture 22
   ret
 __RULE_FLOATLITERAL:
   call __RULE___prefix
-  catch __FORGIVE_1373
-__ENDLESS_1372:
+  opencapture 23
+  catch __FORGIVE_1415
+__ENDLESS_1414:
   set 000000000000ff03000000000000000000000000000000000000000000000000
-  partialcommit __ENDLESS_1372
-__FORGIVE_1373:
+  partialcommit __ENDLESS_1414
+__FORGIVE_1415:
   char 2e
   set 000000000000ff03000000000000000000000000000000000000000000000000
-  catch __FORGIVE_1387
-__ENDLESS_1386:
+  catch __FORGIVE_1429
+__ENDLESS_1428:
   set 000000000000ff03000000000000000000000000000000000000000000000000
-  partialcommit __ENDLESS_1386
-__FORGIVE_1387:
+  partialcommit __ENDLESS_1428
+__FORGIVE_1429:
+  closecapture 23
   ret
 __RULE_INTLITERAL:
   call __RULE___prefix
+  opencapture 24
   set 000000000000ff03000000000000000000000000000000000000000000000000
-  catch __FORGIVE_1394
-__ENDLESS_1393:
+  catch __FORGIVE_1443
+__ENDLESS_1442:
   set 000000000000ff03000000000000000000000000000000000000000000000000
-  partialcommit __ENDLESS_1393
-__FORGIVE_1394:
+  partialcommit __ENDLESS_1442
+__FORGIVE_1443:
+  closecapture 24
   ret
 __RULE_BOOLEANLITERAL:
   call __RULE___prefix
-  catch __RIGHTHAND_1397
+  opencapture 25
+  catch __RIGHTHAND_1453
   quad 74727565
-  commit __SUCCESS_1397
-__RIGHTHAND_1397:
+  commit __SUCCESS_1453
+__RIGHTHAND_1453:
   quad 66616c73
   char 65
-__SUCCESS_1397:
+__SUCCESS_1453:
+  closecapture 25
   ret
 __RULE_KW_IMPORT:
   call __RULE___prefix
+  opencapture 26
   quad 696d706f
   char 72
   char 74
+  closecapture 26
   ret
 __RULE_KW_FUNCTION:
   call __RULE___prefix
+  opencapture 27
   quad 66756e63
   quad 74696f6e
+  closecapture 27
   ret
 __RULE_KW_VAR:
   call __RULE___prefix
+  opencapture 28
   char 76
   char 61
   char 72
+  closecapture 28
   ret
 __RULE_KW_WHILE:
   call __RULE___prefix
+  opencapture 29
   quad 7768696c
   char 65
+  closecapture 29
   ret
 __RULE_KW_RETURN:
   call __RULE___prefix
+  opencapture 30
   quad 72657475
   char 72
   char 6e
+  closecapture 30
   ret
 __RULE_KW_IF:
   call __RULE___prefix
+  opencapture 31
   char 69
   char 66
+  closecapture 31
   ret
 __RULE_KW_ELSEIF:
   call __RULE___prefix
+  opencapture 32
   quad 656c7365
   char 69
   char 66
+  closecapture 32
   ret
 __RULE_KW_ELSE:
   call __RULE___prefix
+  opencapture 33
   quad 656c7365
+  closecapture 33
   ret
 __RULE_KW_BREAK:
   call __RULE___prefix
+  opencapture 34
   quad 62726561
   char 6b
+  closecapture 34
   ret
 __RULE_KW_CONTINUE:
   call __RULE___prefix
+  opencapture 35
   quad 636f6e74
   quad 696e7565
+  closecapture 35
   ret
 __RULE_IDENT:
   call __RULE___prefix
+  opencapture 36
   set 0000000000000000feffff87feffff0700000000000000000000000000000000
-  catch __FORGIVE_1493
+  catch __FORGIVE_1626
   counter 1 63
-__COUNTER_1491:
+__COUNTER_1624:
   set 000000000000ff03feffff87feffff0700000000000000000000000000000000
-  partialcommit __USELESS_1495
-__USELESS_1495:
-  condjump 1 __COUNTER_1491
-  commit __FORGIVE_1493
-__FORGIVE_1493:
+  partialcommit __USELESS_1628
+__USELESS_1628:
+  condjump 1 __COUNTER_1624
+  commit __FORGIVE_1626
+__FORGIVE_1626:
+  closecapture 36
   ret
 __RULE_BOPEN:
   call __RULE___prefix
+  opencapture 37
   char 28
+  closecapture 37
   ret
 __RULE_BCLOSE:
   call __RULE___prefix
+  opencapture 38
   char 29
+  closecapture 38
   ret
 __RULE_CBOPEN:
   call __RULE___prefix
+  opencapture 39
   char 7b
+  closecapture 39
   ret
 __RULE_CBCLOSE:
   call __RULE___prefix
+  opencapture 40
   char 7d
+  closecapture 40
   ret
 __RULE_ABOPEN:
   call __RULE___prefix
+  opencapture 41
   char 5b
+  closecapture 41
   ret
 __RULE_ABCLOSE:
   call __RULE___prefix
+  opencapture 42
   char 5d
+  closecapture 42
   ret
 __RULE_ASSIGNS:
   call __RULE___prefix
-  opencapture 22
-  catch __RIGHTHAND_1545
+  opencapture 43
+  catch __RIGHTHAND_1720
   call __RULE_ASSIGN
-  commit __SUCCESS_1545
-__RIGHTHAND_1545:
-  catch __RIGHTHAND_1553
+  commit __SUCCESS_1720
+__RIGHTHAND_1720:
+  catch __RIGHTHAND_1728
   call __RULE_PLUSIS
-  commit __SUCCESS_1553
-__RIGHTHAND_1553:
-  catch __RIGHTHAND_1561
+  commit __SUCCESS_1728
+__RIGHTHAND_1728:
+  catch __RIGHTHAND_1736
   call __RULE_MINIS
-  commit __SUCCESS_1561
-__RIGHTHAND_1561:
-  catch __RIGHTHAND_1569
+  commit __SUCCESS_1736
+__RIGHTHAND_1736:
+  catch __RIGHTHAND_1744
   call __RULE_MULIS
-  commit __SUCCESS_1569
-__RIGHTHAND_1569:
+  commit __SUCCESS_1744
+__RIGHTHAND_1744:
   call __RULE_DIVIS
-__SUCCESS_1569:
-__SUCCESS_1561:
-__SUCCESS_1553:
-__SUCCESS_1545:
-  closecapture 22
+__SUCCESS_1744:
+__SUCCESS_1736:
+__SUCCESS_1728:
+__SUCCESS_1720:
+  closecapture 43
   ret
 __RULE_ASSIGN:
   call __RULE___prefix
+  opencapture 44
   char 3d
+  closecapture 44
   ret
 __RULE_PLUSIS:
   call __RULE___prefix
+  opencapture 45
   char 2b
   char 3d
+  closecapture 45
   ret
 __RULE_MINIS:
   call __RULE___prefix
+  opencapture 46
   char 2d
   char 3d
+  closecapture 46
   ret
 __RULE_MULIS:
   call __RULE___prefix
+  opencapture 47
   char 2a
   char 3d
+  closecapture 47
   ret
 __RULE_DIVIS:
   call __RULE___prefix
+  opencapture 48
   char 2f
   char 3d
+  closecapture 48
   ret
 __RULE_EQUALS:
   call __RULE___prefix
+  opencapture 49
   char 3d
   char 3d
+  closecapture 49
   ret
 __RULE_NEQUALS:
   call __RULE___prefix
+  opencapture 50
   char 21
   char 3d
+  closecapture 50
   ret
 __RULE_LT:
   call __RULE___prefix
+  opencapture 51
   char 3c
-  catch __PREFIX_1640
+  catch __PREFIX_1871
   char 3d
   failtwice
-__PREFIX_1640:
+__PREFIX_1871:
+  closecapture 51
   ret
 __RULE_GT:
   call __RULE___prefix
+  opencapture 52
   char 3e
-  catch __PREFIX_1654
+  catch __PREFIX_1892
   char 3d
   failtwice
-__PREFIX_1654:
+__PREFIX_1892:
+  closecapture 52
   ret
 __RULE_LTEQ:
   call __RULE___prefix
+  opencapture 53
   char 3c
   char 3d
+  closecapture 53
   ret
 __RULE_GTEQ:
   call __RULE___prefix
+  opencapture 54
   char 3e
   char 3d
+  closecapture 54
   ret
 __RULE_SEMICOLON:
   call __RULE___prefix
+  opencapture 55
   char 3b
+  closecapture 55
   ret
 __RULE_COLON:
   call __RULE___prefix
+  opencapture 56
   char 3a
+  closecapture 56
   ret
 __RULE_POW:
   call __RULE___prefix
+  opencapture 57
   char 2a
   char 2a
+  closecapture 57
   ret
 __RULE_MUL:
   call __RULE___prefix
+  opencapture 58
   char 2a
-  catch __PREFIX_1703
+  catch __PREFIX_1983
   char 2a
   failtwice
-__PREFIX_1703:
+__PREFIX_1983:
+  closecapture 58
   ret
 __RULE_DIV:
   call __RULE___prefix
+  opencapture 59
   char 2f
-  catch __PREFIX_1717
+  catch __PREFIX_2004
   char 3d
   failtwice
-__PREFIX_1717:
+__PREFIX_2004:
+  closecapture 59
   ret
 __RULE_ADD:
   call __RULE___prefix
+  opencapture 60
   char 2b
-  catch __PREFIX_1731
+  catch __PREFIX_2025
   set 0000000000080020000000000000000000000000000000000000000000000000
   failtwice
-__PREFIX_1731:
+__PREFIX_2025:
+  closecapture 60
   ret
 __RULE_SUB:
   call __RULE___prefix
+  opencapture 61
   char 2d
-  catch __PREFIX_1745
+  catch __PREFIX_2046
   set 0000000000200020000000000000000000000000000000000000000000000000
   failtwice
-__PREFIX_1745:
+__PREFIX_2046:
+  closecapture 61
   ret
 __RULE_INC:
   call __RULE___prefix
+  opencapture 62
   char 2b
   char 2b
+  closecapture 62
   ret
 __RULE_DEC:
   call __RULE___prefix
+  opencapture 63
   char 2d
   char 2d
+  closecapture 63
   ret
 __RULE_AND:
   call __RULE___prefix
+  opencapture 64
   char 26
   char 26
+  closecapture 64
   ret
 __RULE_OR:
   call __RULE___prefix
+  opencapture 65
   char 7c
   char 7c
+  closecapture 65
   ret
 __RULE_BITAND:
   call __RULE___prefix
+  opencapture 66
   char 26
-  catch __PREFIX_1787
+  catch __PREFIX_2123
   set 0000000040000020000000000000000000000000000000000000000000000000
   failtwice
-__PREFIX_1787:
+__PREFIX_2123:
+  closecapture 66
   ret
 __RULE_BITOR:
   call __RULE___prefix
+  opencapture 67
   char 7c
-  catch __PREFIX_1801
+  catch __PREFIX_2144
   set 0000000000000020000000000000001000000000000000000000000000000000
   failtwice
-__PREFIX_1801:
+__PREFIX_2144:
+  closecapture 67
   ret
 __RULE_BITXOR:
   call __RULE___prefix
+  opencapture 68
   char 5e
-  catch __PREFIX_1815
+  catch __PREFIX_2165
   char 3d
   failtwice
-__PREFIX_1815:
+__PREFIX_2165:
+  closecapture 68
   ret
 __RULE_NOT:
   call __RULE___prefix
+  opencapture 69
   char 21
-  catch __PREFIX_1829
+  catch __PREFIX_2186
   char 3d
   failtwice
-__PREFIX_1829:
+__PREFIX_2186:
+  closecapture 69
   ret
 __RULE_COMMA:
   call __RULE___prefix
+  opencapture 70
   char 2c
+  closecapture 70
   ret
--- t/single_00018.tst
+++ t/single_00018.tst
@@ -22,21 +22,22 @@ FUNCDECL       <- KW_FUNCTION { IDENT }
                    BOPEN ( IDENT ( COMMA IDENT )* )? BCLOSE
                    CBOPEN ( LOWSTMT )* CBCLOSE
 IFSTMT         <- KW_IF BOPEN EXPRESSION BCLOSE CBOPEN ( LOWSTMT )* CBCLOSE
-                  ( KW_ELSEIF BOPEN EXPRESSION BCLOSE CBOPEN ( LOWSTMT )* CBCLOSE )*
+                  ( KW_ELSEIF BOPEN EXPRESSION BCLOSE
+                    CBOPEN ( LOWSTMT )* CBCLOSE )*
                   ( KW_ELSE CBOPEN ( LOWSTMT )* CBCLOSE )?
 WHILESTMT      <- KW_WHILE BOPEN EXPRESSION BCLOSE CBOPEN ( LOWSTMT )* CBCLOSE
 RETURNSTMT     <- KW_RETURN EXPRESSION SEMICOLON
 OTHERSTMT      <- ( KW_BREAK / KW_CONTINUE ) SEMICOLON
-VARDECL        <- KW_VAR { IDENT } ( ASSIGN EXPRESSION )? SEMICOLON
-ASSIGNMENT     <- { IDENT } ASSIGNS EXPRESSION SEMICOLON
+VARDECL        <- KW_VAR IDENT ( ASSIGN EXPRESSION )? SEMICOLON
+ASSIGNMENT     <- IDENT ASSIGNS EXPRESSION SEMICOLON
 
-EXPRESSION     <- EXPR2 ({ AND / OR } EXPR2 )*
-EXPR2          <- EXPR3 ({ BITAND / BITOR / BITXOR } EXPR3 )*
-EXPR3          <- EXPR4 ({ EQUALS / NEQUALS / LTEQ / LT / GTEQ / GT } EXPR4 )*
-EXPR4          <- EXPR5 ({ POW / MUL / DIV } EXPR5 )*
-EXPR5          <- EXPR6 ({ ADD / SUB } EXPR6 )*
-EXPR6          <- ( UNARY )* TERM
-UNARY          <- { NOT / INC / DEC }
+EXPRESSION     <- EXPR2 (( AND / OR ) EXPR2 )*
+EXPR2          <- EXPR3 (( BITAND / BITOR / BITXOR ) EXPR3 )*
+EXPR3          <- EXPR4 (( EQUALS / NEQUALS / LTEQ / LT / GTEQ / GT ) EXPR4 )*
+EXPR4          <- EXPR5 (( POW / MUL / DIV ) EXPR5 )*
+EXPR5          <- EXPR6 (( ADD / SUB ) EXPR6 )*
+EXPR6          <- { UNARY }* { TERM }
+UNARY          <- NOT / INC / DEC
 TERM           <- { LITERAL } /
                   { IDENT } BOPEN ( EXPRESSION ( COMMA EXPRESSION )* )? BCLOSE /
                   { IDENT } /
@@ -48,63 +49,64 @@ LITERAL        <- STRINGLITERAL /
                   FLOATLITERAL /
                   INTLITERAL /
                   BOOLEANLITERAL
-STRINGLITERAL  <- '\'' ( '\\' ([nrtv\\'] / [0-9]^3) / [^'\\] )* '\''
-HASHLITERAL    <- CBOPEN ( HASHELT ( COMMA HASHELT )* )? CBCLOSE
-HASHELT        <- HASHKEY COLON HASHVALUE
-HASHKEY        <- TERM
-HASHVALUE      <- TERM
-LISTLITERAL    <- ABOPEN ( LISTELT ( COMMA LISTELT )* )? ABCLOSE
-LISTELT        <- TERM
-FLOATLITERAL   <- [0-9]* '.' [0-9]+
-INTLITERAL     <- [0-9]+
-BOOLEANLITERAL <- 'true' / 'false'
 
-KW_IMPORT      <- 'import'
-KW_FUNCTION    <- 'function'
-KW_VAR         <- 'var'
-KW_WHILE       <- 'while'
-KW_RETURN      <- 'return'
-KW_IF          <- 'if'
-KW_ELSEIF      <- 'elseif'
-KW_ELSE        <- 'else'
-KW_BREAK       <- 'break'
-KW_CONTINUE    <- 'continue'
+STRINGLITERAL  <- { '\'' ( '\\' ([nrtv\\'] / [0-9]^3) / [^'\\] )* '\'' }
+HASHLITERAL    <- { CBOPEN ( HASHELT ( COMMA HASHELT )* )? CBCLOSE }
+HASHELT        <- { HASHKEY COLON HASHVALUE }
+HASHKEY        <- { TERM }
+HASHVALUE      <- { TERM }
+LISTLITERAL    <- { ABOPEN ( LISTELT ( COMMA LISTELT )* )? ABCLOSE }
+LISTELT        <- { TERM }
+FLOATLITERAL   <- { [0-9]* '.' [0-9]+ }
+INTLITERAL     <- { [0-9]+ }
+BOOLEANLITERAL <- { 'true' / 'false' }
 
-IDENT          <- [a-zA-Z_][a-zA-Z0-9_]^0-63
-BOPEN          <- '('
-BCLOSE         <- ')'
-CBOPEN         <- '{'
-CBCLOSE        <- '}'
-ABOPEN         <- '['
-ABCLOSE        <- ']'
+KW_IMPORT      <- { 'import' }
+KW_FUNCTION    <- { 'function' }
+KW_VAR         <- { 'var' }
+KW_WHILE       <- { 'while' }
+KW_RETURN      <- { 'return' }
+KW_IF          <- { 'if' }
+KW_ELSEIF      <- { 'elseif' }
+KW_ELSE        <- { 'else' }
+KW_BREAK       <- { 'break' }
+KW_CONTINUE    <- { 'continue' }
+
+IDENT          <- { [a-zA-Z_][a-zA-Z0-9_]^0-63 }
+BOPEN          <- { '(' }
+BCLOSE         <- { ')' }
+CBOPEN         <- { '{' }
+CBCLOSE        <- { '}' }
+ABOPEN         <- { '[' }
+ABCLOSE        <- { ']' }
 ASSIGNS        <- { ASSIGN / PLUSIS / MINIS / MULIS / DIVIS }
-ASSIGN         <- '='
-PLUSIS         <- '+='
-MINIS          <- '-='
-MULIS          <- '*='
-DIVIS          <- '/='
-EQUALS         <- '=='
-NEQUALS        <- '!='
-LT             <- '<' !'='
-GT             <- '>' !'='
-LTEQ           <- '<='
-GTEQ           <- '>='
-SEMICOLON      <- ';'
-COLON          <- ':'
-POW            <- '**'
-MUL            <- '*' !'*'
-DIV            <- '/' !'='
-ADD            <- '+' ![+=]
-SUB            <- '-' ![-=]
-INC            <- '++'
-DEC            <- '--'
-AND            <- '&&'
-OR             <- '||'
-BITAND         <- '&' ![&=]
-BITOR          <- '|' ![|=]
-BITXOR         <- '^' !'='
-NOT            <- '!' !'='
-COMMA          <- ','
+ASSIGN         <- { '=' }
+PLUSIS         <- { '+=' }
+MINIS          <- { '-=' }
+MULIS          <- { '*=' }
+DIVIS          <- { '/=' }
+EQUALS         <- { '==' }
+NEQUALS        <- { '!=' }
+LT             <- { '<' !'=' }
+GT             <- { '>' !'=' }
+LTEQ           <- { '<=' }
+GTEQ           <- { '>=' }
+SEMICOLON      <- { ';' }
+COLON          <- { ':' }
+POW            <- { '**' }
+MUL            <- { '*' !'*' }
+DIV            <- { '/' !'=' }
+ADD            <- { '+' ![+=] }
+SUB            <- { '-' ![\-=] }
+INC            <- { '++' }
+DEC            <- { '--' }
+AND            <- { '&&' }
+OR             <- { '||' }
+BITAND         <- { '&' ![&=] }
+BITOR          <- { '|' ![|=] }
+BITXOR         <- { '^' !'=' }
+NOT            <- { '!' !'=' }
+COMMA          <- { ',' }
 
 -- Input:
 

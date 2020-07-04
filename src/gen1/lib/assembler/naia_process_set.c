8f25cb8e8a12a04ec59709a87bb780307cc5aa8d
KJ <kees.jan.hermans@gmail.com>
2020-07-04 11:54:49 +0200

    Issue warning when producing unusual sets

--- src/gen1/lib/assembler/naia_process_set.c
+++ src/gen1/lib/assembler/naia_process_set.c
@@ -21,12 +21,20 @@ NAIG_ERR_T naia_process_set
   uint32_t opcode[ 9 ] = { htonl(OPCODE_SET) };
   unsigned char* set = (unsigned char*)(&(opcode[ 1 ]));
   unsigned c;
+  unsigned char none[ 32 ] = { 0 };
+  unsigned char all[ 32 ] = { 255 };
 
   for (c=0; c < 32; c++) {
     set[ c ] = hexcodon(
       naia->assembly[ naia->captures->actions[ i+1 ].start + (c*2) ],
       naia->assembly[ naia->captures->actions[ i+1 ].start + (c*2) + 1 ]
     );
+  }
+  if (0 == memcmp(set, none, sizeof(none))) {
+    fprintf(stderr, "WARNING: Set matches no input byte (empty set).\n");
+  }
+  if (0 == memcmp(set, all, sizeof(all))) {
+    fprintf(stderr, "WARNING: Set matches all input bytes (full set).\n");
   }
   CHECK(naia->write(opcode, sizeof(opcode), naia->write_arg));
   return NAIG_OK;

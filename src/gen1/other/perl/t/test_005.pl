889c85b2277bacfc6857266059823eb158c1fcee
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2021-12-13 19:35:22 +0100

    New tests and quieter too

--- src/gen1/other/perl/t/test_005.pl
+++ src/gen1/other/perl/t/test_005.pl
@@ -34,8 +34,28 @@ EOS
 ;
 
 my $json = <<'EOS'
-
-{ "foo" : { "bar" : "test" } }
+{
+  "glossary": {
+    "title": "example glossary",
+    "GlossDiv": {
+      "title": "S",
+      "GlossList": {
+        "GlossEntry": {
+          "ID": "SGML",
+          "SortAs": "SGML",
+          "GlossTerm": "Standard Generalized Markup Language",
+          "Acronym": "SGML",
+          "Abbrev": "ISO 8879:1986",
+          "GlossDef": {
+            "para": "A meta-markup language.",
+            "GlossSeeAlso": ["GML", "XML"]
+          },
+          "GlossSee": "markup"
+        }
+      }
+    }
+  }
+}
 
 EOS
 ;
@@ -46,7 +66,7 @@ my $output = eval { $parser->run($json) };
 if ($output) {
   print "Ok\n";
 } else {
-  print "NOK; $@\n";
+  print "NOK\n";
 }
 
 1;

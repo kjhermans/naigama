595f298c5cf87bfb7e5f24823d47fed211ad2186
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2021-12-23 18:11:50 +0100

    Introducing the possibility of compiler errors.

---
+++ src/gen1/other/java/src/lib/naigama/compiler/NaigamaCompilerError.java
@@ -0,0 +1,13 @@
+package lib.naigama.compiler;
+
+import lib.naigama.NaigamaException;
+
+public class NaigamaCompilerError
+  extends NaigamaException
+{
+  public NaigamaCompilerError
+    (String e)
+  {
+    super(e);
+  }
+}

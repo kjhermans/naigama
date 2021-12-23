ec14f5b26b0a0b56720753f557596c107046ea9d
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2021-12-23 18:12:21 +0100

    Introducing the possibility of variable namespaces.

--- src/gen1/other/java/src/lib/naigama/compiler/CompilerState.java
+++ src/gen1/other/java/src/lib/naigama/compiler/CompilerState.java
@@ -3,16 +3,18 @@ package lib.naigama.compiler;
 import java.util.Hashtable;
 
 import lib.naigama.engine.TreeNode;
+import lib.naigama.NaigamaException;
 
 class CompilerState
 {
-  CompilerOptions               options;
-  int                           counter = 0;
-  int                           slot = 0;
-  int                           reg = 0;
-  TreeNode                      currentrule;
-  int                           currentcapture = 0;
-  Hashtable<TreeNode,Integer>   capturemap = new Hashtable<TreeNode,Integer>();
+  CompilerOptions                     options;
+  int                                 counter = 0;
+  int                                 reg = 0;
+  TreeNode                            currentrule;
+
+  private int                         currentcapture = 0;
+  private Hashtable<TreeNode,Integer> capturemap = new Hashtable<TreeNode,Integer>();
+  private Hashtable<String,Integer>   varmap = new Hashtable<String,Integer>();
 
   int getCapture
     (TreeNode t)
@@ -21,6 +23,23 @@ class CompilerState
     if (i == null) {
       i = new Integer(currentcapture++);
       capturemap.put(t, i);
+    }
+    return i.intValue();
+  }
+
+  void varPut
+    (String varname, int slot)
+  {
+    varmap.put(varname, new Integer(slot));
+  }
+
+  int varGet
+    (String varname)
+    throws NaigamaException
+  {
+    Integer i = varmap.get(varname);
+    if (i == null) {
+      throw new NaigamaCompilerError("Cannot resolve variable reference '" + varname + "'");
     }
     return i.intValue();
   }

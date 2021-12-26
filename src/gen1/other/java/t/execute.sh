431e1964f4c54548dc1faf6919cf91d50b886e49
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2021-12-26 21:42:06 +0100

    All java tests with full debugging on.

--- src/gen1/other/java/t/execute.sh
+++ src/gen1/other/java/t/execute.sh
@@ -5,7 +5,7 @@ TESTFILES=`ls ../../../t/multi_*.tst`
 for TESTFILE in $TESTFILES; do
   perl ../../../bin/multitest.pl \
     $TESTFILE \
-    "../src/jnaic -i GRAMMAR -o ASM" \
-    "../src/jnaia -i ASM -o BYTECODE" \
-    "../src/jnaie -c BYTECODE -i INPUT -D"
+    "../src/jnaic -D -i GRAMMAR -o ASM" \
+    "../src/jnaia -D -i ASM -o BYTECODE" \
+    "../src/jnaie -D -c BYTECODE -i INPUT -D"
 done

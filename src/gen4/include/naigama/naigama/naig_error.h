c7e1eafb144d284a86238e9df63e3741edc3fb27
Kees-Jan Hermans <kees.jan.hermans@gmail.com>
2023-03-15 18:49:29 +0100

    Change in return value.

--- src/gen4/include/naigama/naigama/naig_error.h
+++ src/gen4/include/naigama/naigama/naig_error.h
@@ -61,6 +61,7 @@ typedef struct { int code; char* message; } NAIG_ERR_T;
 #define NAIG_ERR_REPLACE        (NAIG_ERR_T){ .code = 12, .message = 0 }
 #define NAIG_ERR_LIMITEDCALL    (NAIG_ERR_T){ .code = 13, .message = 0 }
 #define NAIG_ERR_LABEL          (NAIG_ERR_T){ .code = 14, .message = 0 }
+#define NAIG_ERR_IO             (NAIG_ERR_T){ .code = 15, .message = 0 }
 
 // Macroes to deal with errors
 
